d6f3fa732b697b0cc1d1e9fe29b824b0f65e227b
Thainguyen812 <167802348+Thainguyen812@users.noreply.github.com>
2025-06-19 13:13:27 +0700

    Add files via upload

---
+++ Module 4/script.js
@@ -0,0 +1,24 @@
+
+function pageLoaded() {
+    console.log('Trang đã tải xong');
+}
+
+
+function mouseoverFunction() {
+    console.log('Mouse over image');
+}
+
+
+function mouseleaveFunction() {
+    console.log('Mouse left image');
+}
+
+
+function onFocusFunction() {
+    console.log('Image is focused');
+}
+
+
+function onBlurFunction() {
+    console.log('Image lost focus');
+}
